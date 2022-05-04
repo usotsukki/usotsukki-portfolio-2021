68e8db857d5b019fa8864655d05def61e133436a
usotsukki <v.shk.business@gmail.com>
2022-05-04 15:16:58 +0300

    p5

--- src/p5thing.js
+++ src/p5thing.js
@@ -1,11 +1,13 @@
-const particles = [];
-let Speed = 0.7;
+const fps = 40;
+let Speed = 1.7;
 let particleSize = 7;
 let backgroundColor = "#111116";
 //particle speed
 let smin = 0 - Speed;
 let smax = Speed;
-const fps = 60;
+
+const particles = [];
+
 function setup() {
 	// performance related
 	frameRate(fps);
@@ -63,25 +65,28 @@ class Particle {
 	checkParticles(particles) {
 		particles.forEach((p, i, arr) => {
 			const d = dist(this.pos.x, this.pos.y, p.pos.x, p.pos.y);
-			if (d < 150) {
+			if (d < 140) {
 				const prev = arr[i - 1 > -1 ? i - 1 : 0];
-				prev.connected *= 1.1;
-				p.connected *= 1.1;
-
 				let color = `255,255,255`;
-				if (p.connected > 1.8) color = `10,189,198`;
-				if (p.connected > 3) color = `255,59,148`;
-				if (p.connected > 5) color = `255,0,0`;
-				const strokeOpacity = p.connected * 0.11;
+
+				prev.connected *= 1.3;
+				p.connected *= 1.3;
 
 				setTimeout(() => {
 					p.connected = 1;
 					prev.connected = 1;
-				}, 1000 / 16);
+				}, 25);
+
+				const strokeOpacity = p.connected * 0.05;
+
+				if (p.connected > 5) color = `10,189,198`;
+				if (p.connected > 17) color = `255,59,148`;
+				if (p.connected > 55) color = `255,0,0`;
+
 				stroke(`rgba(${color},${strokeOpacity})`);
+
 				line(this.pos.x, this.pos.y, p.pos.x, p.pos.y);
 				return;
-			} else {
 			}
 		});
 	}
