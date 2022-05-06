ce3bd3f5244a8295cc03b273a046d752ff4f6765
usotsukki <v.shk.business@gmail.com>
2022-05-06 20:36:29 +0300

    links

--- src/app.js
+++ src/app.js
@@ -6,3 +6,11 @@ window.onload = () => {
 	window.scrollTo(0, 0);
 	scrollSetup();
 };
+
+document.getElementById("prj").addEventListener("click", () => {
+	window.scrollBy({
+		top: windowHeight * 1.5,
+		left: 270,
+		behavior: "smooth",
+	});
+});
